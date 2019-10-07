7c0950604d23d742a8753d0e0d998168e31983f0
Anthony Molina <antonio-molina@att.net>
2019-10-07 02:37:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @student_hash = {:name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url }
+    @student_hash = { "name" => name, "location" => location, "twitter" => twitter, "linkedin" => linkedin, "github" => github, "blog" => blog, "profile_quote" => profile_quote, "bio" => bio, "profile_url" => profile_url }
     @@all << self
   end
 
